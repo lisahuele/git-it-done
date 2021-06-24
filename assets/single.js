414928cd7df0e241b7066836365e424589245c22
lisahuele <lisahuele@gmail.com>
2021-06-24 13:28:23 -0400

    limit warning - added

--- assets/single.js
+++ assets/single.js
@@ -1,4 +1,19 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+
+//action 3
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+  
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+  };
 
 //action 2
 var displayIssues = function(issues) {
@@ -51,6 +66,10 @@ var getRepoIssues = function(repo) {
                 //pass response data to DOM function
                 displayIssues(data);
             });
+            //check if api has paginated issues
+            if (response.headers.get("link")) {
+                displayWarning(repo);
+            }
         }
         else {
             alert("There was a problem with your request!")
